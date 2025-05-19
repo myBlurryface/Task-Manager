6f5bf3daa1c9d65392e31107d8a5ccab15435047
Константин Лозицкий <konstantinlozickij@MacBook-Pro-Konstantin.local>
2025-05-19 06:00:41 +0300

    Added nginx

--- Dockerfile
+++ Dockerfile
@@ -10,5 +10,7 @@ COPY requirements.txt .
 RUN pip install --upgrade pip && pip install --no-cache-dir -r requirements.txt
 COPY . .
 
-EXPOSE 8000
+RUN python manage.py collectstatic --noinput
+
+EXPOSE 80
 EXPOSE 6379
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-yn97hx&f8m39qz3iy1a2g8$y#s^+jcz^kmf)9)or2)#@(@$6hv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'task-manager', 'nginx']
 
 
 # Application definition
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
